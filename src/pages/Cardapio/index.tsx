58509a856b4a545863ab8b3ebb85db17b65df968
kelly <kellydakine@gmail.com>
2022-09-13 22:12:40 -0300

    Inserindo a logo do site

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,13 @@
 import styles from "./Cardapio.module.scss";
+// import logo from "../../assets/logo.svg";
+import {ReactComponent as Logo} from "../../assets/logo.svg"
 
 export default function Cardapio() {
     return (
         <main>
             <nav className={styles.menu}>
-                Aluroni
+                <Logo/>
+                {/* <img src={logo} alt="logo da aluroni" /> */}
             </nav>
         </main>
     );
