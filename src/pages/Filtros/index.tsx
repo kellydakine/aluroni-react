c9e9f277ac1557702b2300993d65f951c620c83b
kelly <kellydakine@gmail.com>
2022-09-14 17:24:38 -0300

    Criando o arquivo json para filtrar as opções

--- src/pages/Filtros/index.tsx
+++ src/pages/Filtros/index.tsx
@@ -1,7 +1,13 @@
+import filtros from "./filtros.json";
+
 export default function Filtros() {
     return (
         <div>
-            
+            {filtros.map((opcao) => (
+                <button>
+                    
+                </button>
+            ))}
         </div>
     )
 }
