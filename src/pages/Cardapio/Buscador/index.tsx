1a7ffb6673b9ed8b64a8730c4424e1184ba2a32b
kelly <kellydakine@gmail.com>
2022-09-14 15:57:03 -0300

    Criação da aparte de buscas do site

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+    busca: string,
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+export default function Buscador(props: Props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
