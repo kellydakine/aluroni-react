23592158f35707dce782fd40dd7e67a9d8bdd7a0
kelly <kellydakine@gmail.com>
2022-09-14 19:04:49 -0300

    Criação e estilização do componente filtros

--- src/pages/Filtros/index.tsx
+++ src/pages/Filtros/index.tsx
@@ -1,11 +1,24 @@
 import filtros from "./filtros.json";
+import styles from "./Filtros.module.scss";
+
+type IOpcao = typeof filtros[0];
+// interface opcao {
+//     id: number;
+//     label: string;
+// }
 
 export default function Filtros() {
+
+    function selecionarFiltro(opcao: IOpcao) {
+
+    }
     return (
-        <div>
+        <div className={styles.filtros}>
             {filtros.map((opcao) => (
-                <button>
-                    
+                <button className={styles.filtros__filtro}
+                    key={opcao.id} 
+                    onClick={() => selecionarFiltro(opcao)}>
+                        {opcao.label}
                 </button>
             ))}
         </div>
