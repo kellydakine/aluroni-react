d0f3cbcadd286669bafcc3f211eb85c25bb5255b
kelly <kellydakine@gmail.com>
2022-09-14 23:00:07 -0300

    Inserindo a estilização dos itens do cardapio

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,9 +1,14 @@
+import cardapio from "./itens.json";
+import styles from "./Item.module.scss";
+
 export default function Itens() {
     return(
         <div>
-            {
-            
-            }
+            {cardapio.map((item) => (
+                <div>
+                    {item.title}
+                </div>
+            ))}
         </div>
     )
 }
