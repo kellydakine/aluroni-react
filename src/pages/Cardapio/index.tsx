b8ef144a5453522527642086feaf283b23d6d46d
kelly <kellydakine@gmail.com>
2022-09-14 22:38:00 -0300

    Aplicando funcionalidade para o ordenador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -9,6 +9,7 @@ export default function Cardapio() {
 
     const [busca, setBusca] = useState("");
     const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -30,7 +31,10 @@ export default function Cardapio() {
                         filtro={filtro}
                         setFiltro={setFiltro}
                     />
-                    <Ordenador/>
+                    <Ordenador
+                        ordenador= {ordenador}
+                        setOrdenador={setOrdenador}
+                    />
                 </div>
             </section>
         </main>
